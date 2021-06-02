5351c53a43231de7e8d6be74389fc02a406391f9
phuctd97 <5951071078@st.utc2.edu.vn>
2021-06-03 03:21:14 +0700

    add project to github

---
+++ app/models.py
@@ -0,0 +1,170 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+
+PROVINCE_CHOICE = (
+    ('An Giang', 'An Giang'),
+    ('Bà Rịa - Vũng Tàu', 'Bà Rịa - Vũng Tàu'),
+    ('Bắc Giang', 'Bắc Giang'),
+    ('Bắc Kạn', 'Bắc Kạn'),
+    ('Bạc Liêu', 'Bạc Liêu'),
+    ('Bắc Ninh', 'Bắc Ninh'),
+    ('Bến Tre', 'Bến Tre'),
+    ('Bình Định', 'Bình Định'),
+    ('Bình Dương', 'Bình Dương'),
+    ('Bình Phước', 'Bình Phước'),
+    ('Bình Thuận', 'Bình Thuận'),
+    ('Cà Mau', 'Cà Mau'),
+    ('Cao Bằng', 'Cao Bằng'),
+    ('Đắk Lắk', 'Đắk Lắk'),
+    ('Đắk Nông', 'Đắk Nông'),
+    ('Điện Biên', 'Điện Biên'),
+    ('Đồng Nai', 'Đồng Nai'),
+    ('Đồng Tháp', 'Đồng Tháp'),
+    ('Gia Lai', 'Gia Lai'),
+    ('Hà Giang', 'Hà Giang'),
+    ('Hà Nam', 'Hà Nam'),
+    ('Hà Tĩnh', 'Hà Tĩnh'),
+    ('Hải Dương', 'Hải Dương'),
+    ('Hậu Giang', 'Hậu Giang'),
+    ('Hòa Bình', 'Hòa Bình'),
+    ('Hưng Yên', 'Hưng Yên'),
+    ('Khánh Hòa', 'Khánh Hòa'),
+    ('Kiên Giang', 'Kiên Giang'),
+    ('Kon Tum', 'Kon Tum'),
+    ('Lai Châu', 'Lai Châu'),
+    ('Lâm Đồng', 'Lâm Đồng'),
+    ('Lạng Sơn', 'Lạng Sơn'),
+    ('Lào Cai', 'Lào Cai'),
+    ('Long An', 'Long An'),
+    ('Nam Định', 'Nam Định'),
+    ('Nghệ An', 'Nghệ An'),
+    ('Ninh Bình', 'Ninh Bình'),
+    ('Ninh Thuận', 'Ninh Thuận'),
+    ('Phú Thọ', 'Phú Thọ'),
+    ('Quảng Bình', 'Quảng Bình'),
+    ('Quảng Nam', 'Quảng Nam'),
+    ('Quảng Ngãi', 'Quảng Ngãi'),
+    ('Quảng Ninh', 'Quảng Ninh'),
+    ('Quảng Trị', 'Quảng Trị'),
+    ('Sóc Trăng', 'Sóc Trăng'),
+    ('Sơn La', 'Sơn La'),
+    ('Tây Ninh', 'Tây Ninh'),
+    ('Thái Bình', 'Thái Bình'),
+    ('Thái Nguyên', 'Thái Nguyên'),
+    ('Thanh Hóa', 'Thanh Hóa'),
+    ('Thừa Thiên Huế', 'Thừa Thiên Huế'),
+    ('Tiền Giang', 'Tiền Giang'),
+    ('Trà Vinh', 'Trà Vinh'),
+    ('Tuyên Quang', 'Tuyên Quang'),
+    ('Vĩnh Long', 'Vĩnh Long'),
+    ('Vĩnh Phúc', 'Vĩnh Phúc'),
+    ('Yên Bái', 'Yên Bái'),
+    ('Phú Yên', 'Phú Yên'),
+    ('Cần Thơ', 'Cần Thơ'),
+    ('Đà Nẵng', 'Đà Nẵng'),
+    ('Hải Phòng', 'Hải Phòng'),
+    ('Hà Nội', 'Hà Nội'),
+    ('TP HCM', 'TP HCM')
+)
+
+
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    district = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    province = models.CharField(choices=PROVINCE_CHOICE, max_length=100)
+
+    def __str__(self):
+        return str(self.id)
+
+
+BRAND_CHOICES = (
+    ('Asus', 'Asus'),
+    ('Lenovo', 'Lenovo'),
+    ('Macbook', 'Macbook'),
+    ('Acer', 'Acer'),
+    ('Dell', 'Dell'),
+    ('HP', 'HP'),
+    ('MSI','MSI'),
+    ('LG','LG'),
+    ('SN','Sony'),
+    ('AP','Apple'),
+    ('SS','Samsung'),
+    ('LGT','Logitech'),
+    ('TCN','Tucano'),
+    ('TOM','Tomtoc'),
+    ('WW','Wiwu'),
+    ('MZ','Mozard'),
+    ('PTO','Patriot'),
+    ('KL','Kalevv'),
+    ('TEA','Teamgroup'),
+    ('GG','Gigabyte'),
+    ('KST','Kingston')
+    
+)
+
+CATEGORY_CHOICES = (
+    ('LT', 'Laptop'),
+    ('C','Components'),
+    ('ACC', 'Accessories'),
+)
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    desception = models.TextField()
+    brand = models.CharField(choices =BRAND_CHOICES ,max_length=9)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=3)
+    product_image = models.ImageField(upload_to='productimg')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.selling_price
+
+STATUS_CHOICE = (
+    ('Accepted', 'Accepted'),
+    ('Packed', 'Packed'),
+    ('On the way', 'On the way'),
+    ('Delivered', 'Delivared'),
+    ('Cancel', 'Cancel')
+)
+
+
+class OrderPlaced(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=50, choices=STATUS_CHOICE, default='Pending')
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.selling_price
---
+++ app/views.py
@@ -0,0 +1,251 @@
+from django.http.response import JsonResponse
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import BRAND_CHOICES, Customer, Cart, Product, OrderPlaced
+from .forms import CustomerRegistrationForm, CustomerProfileForm
+from django.contrib import messages
+from django.db.models import Q
+from django.http import JsonResponse
+from app import forms
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class ProductView(View):
+    def get(self, request):
+        totalitem = 0
+        laptop = Product.objects.filter(category='LT')
+        component = Product.objects.filter(category='C')
+        accessories = Product.objects.filter(category='ACC')
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
+        return render(request, 'app/home.html',
+                      {'laptop': laptop, 'component': component, 'accessories': accessories, 'totalitem': totalitem})
+
+
+class ProductDetailView(View):
+    def get(self, request, pk):
+        totalitem = 0
+        product = Product.objects.get(pk=pk,)
+        item_already_in_cart = False
+        if request.user.is_authenticated:
+            totalitem = len(Cart.objects.filter(user=request.user))
+            item_already_in_cart = Cart.objects.filter(
+                Q(product=product.id) & Q(user=request.user)).exists()
+        return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart': item_already_in_cart, 'totalitem': totalitem})
+
+
+@login_required
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect('/cart')
+
+
+@login_required
+def show_cart(request):
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0.0
+        shipping_amount = 30000
+        total_amount = 0.0
+        cart_product = [p for p in Cart.objects.all() if p.user == user]
+    if cart_product:
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.selling_price)
+            amount += tempamount
+            totalamount = amount + shipping_amount
+        return render(request, 'app/addtocart.html', {'carts': cart, 'totalamount': totalamount, 'amount': amount})
+    else:
+        return render(request, 'app/emptycart.html')
+
+
+def plus_cart(request):
+    if request.method == "GET":
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity += 1
+        c.save()
+        amount = 0.0
+        shipping_amount = 30000
+        cart_product = [p for p in Cart.objects.all() if p.user ==
+                        request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.selling_price)
+            amount += tempamount
+
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+        }
+        return JsonResponse(data)
+
+
+def minus_cart(request):
+    if request.method == "GET":
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity -= 1
+        c.save()
+        amount = 0.0
+        shipping_amount = 30000
+        cart_product = [p for p in Cart.objects.all() if p.user ==
+                        request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.selling_price)
+            amount += tempamount
+
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+        }
+        return JsonResponse(data)
+
+
+def remove_cart(request):
+    if request.method == "GET":
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        amount = 0.0
+        shipping_amount = 30000
+        cart_product = [p for p in Cart.objects.all() if p.user ==
+                        request.user]
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.selling_price)
+            amount += tempamount
+        data = {
+            'amount': amount,
+            'totalamount': amount + shipping_amount
+        }
+        return JsonResponse(data)
+
+
+def buy_now(request):
+    return render(request, 'app/buynow.html')
+
+
+@login_required
+def address(request):
+    add = Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html', {'add': add, 'active': 'btn-primary'})
+
+
+@login_required
+def orders(request):
+    op = OrderPlaced.objects.filter(user=request.user)
+    return render(request, 'app/orders.html', {'orderplaced': op})
+
+# Laptop
+
+
+def laptop(request, data=None):
+    if data == None:
+        laptops = Product.objects.filter(category='LT')
+    elif data == 'Asus' or data == 'Lenovo' or data == 'Macbook' or data == 'Acer' or data == 'Dell' or data == 'HP' or data == 'MSI' or data == 'LG':
+        laptops = Product.objects.filter(category='LT').filter(brand=data)
+    elif data == 'below':
+        laptops = Product.objects.filter(category='LT').filter(
+            discounted_price__lt=20000000)
+    elif data == 'above':
+        laptops = Product.objects.filter(category='LT').filter(
+            discounted_price__gt=20000000)
+    return render(request, 'app/laptop.html', {'laptops': laptops})
+
+# Accessories
+def accessories(request, data=None):
+    if data == None:
+        accessoriess = Product.objects.filter(category='ACC')
+    elif data == 'MZ' or data == 'WW' or data == 'TOM' or data == 'TCN' or data == 'AP' or data == 'LGT' or data == 'Asus' or data == 'SS':
+        accessoriess = Product.objects.filter(category='ACC').filter(brand=data)
+    elif data == 'below':
+        accessoriess = Product.objects.filter(category='ACC').filter(
+            discounted_price__lt=2000000)
+    elif data == 'above':
+        accessoriess = Product.objects.filter(category='ACC').filter(
+            discounted_price__gt=2000000)
+    return render(request, 'app/accessories.html', {'accessoriess': accessoriess})
+
+# Component
+def component(request, data=None):
+    if data == None:
+        components = Product.objects.filter(category='C')
+    elif data == 'TEA' or data == 'SS' or data == 'PTO' or data == 'GG' or data == 'KST':
+        components = Product.objects.filter(category='C').filter(brand=data)
+    elif data == 'below':
+        components = Product.objects.filter(category='C').filter(
+            discounted_price__lt=2000000)
+    elif data == 'above':
+        components = Product.objects.filter(category='C').filter(
+            discounted_price__gt=2000000)
+    return render(request, 'app/component.html', {'components': components})
+
+
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(request, "Registed successfully")
+            form.save()
+        return render(request, 'app/customerregistration.html', {'form': form})
+
+
+@login_required
+def checkout(request):
+    user = request.user
+    add = Customer.objects.filter(user=user)
+    cart_items = Cart.objects.filter(user=user)
+    amount = 0.0
+    shipping_amount = 30000
+    totalamount = 0.0
+    cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+    if cart_product:
+        for p in cart_product:
+            tempamount = (p.quantity * p.product.selling_price)
+            amount += tempamount
+        totalamount = amount + shipping_amount
+    return render(request, 'app/checkout.html', {'add': add, 'totalamount': totalamount, 'cart_items': cart_items})
+
+
+@login_required
+def payment_done(request):
+    user = request.user
+    custid = request.GET.get('custid')
+    customer = Customer.objects.get(id=custid)
+    cart = Cart.objects.filter(user=user)
+    for c in cart:
+        OrderPlaced(user=user, customer=customer,
+                    product=c.product, quantity=c.quantity).save()
+        c.delete()
+    return redirect('orders')
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', {'form': form, 'active': 'btn-primary'})
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            usr = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            district = form.cleaned_data['district']
+            province = form.cleaned_data['province']
+            zipcode = form.cleaned_data['zipcode']
+            reg = Customer(user=usr, name=name, locality=locality,
+                           district=district, province=province, zipcode=zipcode)
+            reg.save()
+            messages.success(request, 'Profile Update Successfully')
+        return render(request, 'app/profile.html', {'form': form, 'active': 'btn-primary'})
